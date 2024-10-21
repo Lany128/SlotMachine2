8a3de884f8bc0c882cc427891c421d6dba5bce4f
Evora <lanyevora@gmail.com>
2024-10-21 10:06:10 +0200

    A direct application of Convert.ToInt32(Console.ReadLine()) was created without first storing the input as a string wish to bet

--- Program.cs
+++ Program.cs
@@ -14,14 +14,51 @@ namespace slotMachine
             Console.WriteLine("Welcome to the Slot Machine Game");
 
             // 2)Player starting balance & amount wishe to play
-            Console.WriteLine(" Please enter the amount you wish to add");
-            string userInput = Console.ReadLine();
-            int userInicialAmount = Convert.ToInt32(userInput);
+            Console.WriteLine("Please enter the amount wished to added: ");
+            //string userInput = Console.ReadLine();
+            int userInicialAmount = Convert.ToInt32(Console.ReadLine());
             
-            Console.WriteLine($"Inicial balance:{userInicialAmount}");
+            Console.WriteLine($"Inicial balance:${userInicialAmount}");
 
-                
-            // 3) Slot result
+            Console.WriteLine("Amount wished to play: ");
+
+            int userWishedAmoutToPlay =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Do you wish to bet ${userWishedAmoutToPlay}? Yes/No");
+            string amountConfirmation = Console.ReadLine().ToLower();
+
+            if (amountConfirmation == "yes")
+
+                int[,] array2D = new int[3, 3];
+
+            Random randomNumber = new Random();
+            /*int slotMachineNum = randomNumber.Next(1,4);*/ //-> 2
+            
+            // Fill the array
+            for (int i = 0; i < 3; i++) // Loop through rows
+                {
+                    for (int j = 0; j < 3; j++)// loop through columns
+                    {
+                        array2D[i, j] = randomNumber.Next(MIN_NUM, MAX_NUM); // Set each element to 1
+                    }
+
+                }
+
+            // Print the 3x3 array
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(array2D[i, j]); // Print elements in a row
+                }
+                Console.WriteLine();// Move to the next row
+            }
+
+
+            if (amountConfirmation == "no")
+            {
+
+            }
 
             // info if the player has won or lose
 
@@ -34,33 +71,9 @@ namespace slotMachine
             //End game
 
 
-            int[,] array2D = new int[3, 3];
+           
 
-            Random randomNumber = new Random();
-            /*int slotMachineNum = randomNumber.Next(1,4);*/ //-> 2
-
-            // 1 2 1
-            // 1 1 3
-            // 3 1 1
-            // Fill the array
-            for (int i = 0; i < 3; i++) // Loop through rows
-            {
-                for (int j = 0; j < 3; j++)// loop through columns
-                {
-                    array2D[i, j] = randomNumber.Next(MIN_NUM, MAX_NUM); // Set each element to 1
-                }
-
-            }
-
-            // Print the 3x3 array
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Console.Write(array2D[i, j]); // Print elements in a row
-                }
-                Console.WriteLine();// Move to the next row
-            }
+            
 
 
 
